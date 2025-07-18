a921f380147e4ae4cc3525ac43720b6a478758cb
rohan700976 <rohansingh700976@gmail.com>
2025-07-18 12:13:49 +0530

    main

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const app = express();
 const cors = require('cors');
-required('dotenv').config();
+require('dotenv').config();
 const PORT = 8000;
 
 app.use(express.json());
